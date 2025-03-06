71878d3a4e6719ea14693f0b640b21a7aa6a2d66
CLemmons77 <caleblego7@gmail.com>
2025-03-06 17:13:41 -0500

    added underline to projects design

--- app/config.ts
+++ app/config.ts
@@ -1,14 +1,14 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
   title: "Caleb Nesbitt",
-  name: "Sirius",
+  name: "Caleb Nesbitt",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "A clean, fast, and lightweight portfolio customized for Caleb Nesbitt and built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
   instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  email: "mailto:calebnesbitt11@gmail.com",
 };
