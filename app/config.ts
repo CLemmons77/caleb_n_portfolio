8dcd3ab743a22f530848e4d07d14d7ecf17468e8
CLemmons77 <caleblego7@gmail.com>
2025-03-06 19:53:21 -0500

    updated baseUrl

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
+  baseUrl: "https://calebnportfolio.vercel.app/",
   title: "Caleb Nesbitt",
   name: "Caleb Nesbitt",
   ogImage: "/photos/Portrait_4.png",
